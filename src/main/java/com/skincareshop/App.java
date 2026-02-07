563cc31c7c048f05b3368e0d0c551c1ba083141b
Socheat-ux <your-email@example.com>
2026-02-07 20:17:56 +0700

    Week 3

--- src/main/java/com/skincareshop/App.java
+++ src/main/java/com/skincareshop/App.java
@@ -1,9 +1,34 @@
 package com.skincareshop;
 
+import com.skincareshop.model.Products;
+import com.skincareshop.model.ProductsItem;
+import com.skincareshop.service.CartService;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        Products serum = new Products("Serum", 10.5);
+        ProductsItem item = new ProductsItem(serum, 2);
+        System.out.println(item);
+        System.out.println("\n");
+
+
+        CartService cart = new CartService();
+        Products cream = new Products("Cream", 12);
+        Products sunscreen = new Products("Sunscreen", 8.5);
+        
+        cart.addProductsItem(cream, 2);
+        cart.addProductsItem(sunscreen, 1);
+        cart.printInfo();
+
+        // Remove
+        cart.removeProductsItem(0);
+        cart.printInfo();
+
+        // Update
+        cart.updateQuantityProductsItem(0, 1);
+        cart.printInfo();
     }
 }
--- src/main/java/com/skincareshop/model/ProductsItem.java
+++ src/main/java/com/skincareshop/model/ProductsItem.java
@@ -1,5 +1,39 @@
 package com.skincareshop.model;
 
 public class ProductsItem {
-    
+    private Products product;
+    private int quantity;
+
+    public ProductsItem(Products product, int quantity){
+        this.product = product;
+        setQuantity(quantity);
+    }
+
+    public void setQuantity(int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.quantity = quantity;
+    }
+
+    public Products getProduct() {
+        return new Products(product.getName(), product.getPrice());
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+
+    public double getTotalPrice(){
+        return quantity * product.getPrice();
+    }
+
+
+
+    @Override
+    public String toString() {
+        return product.getName() + " " + getQuantity() + " ($" + getTotalPrice() + ")";
+    }
 }
+
