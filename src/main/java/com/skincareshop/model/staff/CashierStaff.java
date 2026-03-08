af9ca6c7932a640c059febfe8b0563a526c11b63
Socheat-ux <your-email@example.com>
2026-03-08 19:45:50 +0700

    change

--- src/main/java/com/skincareshop/model/staff/CashierStaff.java
+++ src/main/java/com/skincareshop/model/staff/CashierStaff.java
@@ -12,11 +12,6 @@ public class CashierStaff extends Staff {
         this.setSalary(salary);
     }
 
-    // Overloaded — takes base salary + bonus
-    public void setSalary(float baseSalary, float bonus) {
-        setSalary(baseSalary + bonus);
-    }
-
     @Override
     public boolean can(String action) {
         if (action.equals(SkincareShop.CREATE_ORDER) || action.equals(SkincareShop.CREATE_CUSTOMER)
@@ -57,8 +52,6 @@ public class CashierStaff extends Staff {
                 ", salary=" + salary +
                 '}';
     }
-
-
    
 
 }
